a9123758eeb2ec92dac3ff2da8e052cb1c6a6ad1
SID12g <sid12g@naver.com>
2024-02-01 00:03:18 +0900

    feat: add success Interceptor

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -2,30 +2,33 @@ import {
   Controller,
   Delete,
   Get,
-  HttpException,
   Param,
   ParseIntPipe,
   Patch,
   Post,
   Put,
   UseFilters,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CatsService } from './cats.service';
-import { HttpExceptionFilter } from 'src/http-Exception.filter';
+import { HttpExceptionFilter } from 'src/common/exception/http-Exception.filter';
+import { PositiveIntPipe } from 'src/common/pipes/positiveInt.pipe';
+import { SuccessInterceptor } from 'src/common/interceptors/success.interceptor';
 
 @Controller('cats')
+@UseInterceptors(SuccessInterceptor)
 @UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get()
   getAllCat() {
-    throw new HttpException('api broken', 401);
-    return 'all cat';
+    // throw new HttpException('api broken', 401);
+    return { cats: 'get all cat api' };
   }
 
   @Get(':id')
-  getOneCat(@Param('id', ParseIntPipe) param: number) {
+  getOneCat(@Param('id', ParseIntPipe, PositiveIntPipe) param: number) {
     console.log(param);
     console.log(typeof param);
     return 'one cat';
